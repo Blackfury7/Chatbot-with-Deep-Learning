821359d6cb6f052e9017f92f5168165c010a776a
divyanshu <divyanshusharmabb07@gmail.com>
2021-02-23 23:54:12 +0530

    part 2

--- script.py
+++ script.py
@@ -21,11 +21,39 @@ for intent in data['intents']:
     for pattern in intent['patterns']:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs_x.append(wrds)
+        docs_x.append(pattern)
         docs_y.append(intent["tag"])
         
     if intent['tag'] not in labels:
         labels.append(intent['tag'])
 
+words = [stemmer.stem(w.lower()) for w in words]
+words = sorted(list(set(words)))
 
+labels = sorted(labels)
+
+training =[]
+output = []
+
+out_empty = [0 for _ in range(len(classes))]
+
+for x, doc in enumerate(doc_x):
+    bag = []
+
+    wrds = [stemmer.stem(w) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(doc_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = numpy.array(training)
+output = np.array(output)
 
