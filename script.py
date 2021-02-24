15f3652dbdacf424f21680de6773585b659d7de3
divyanshu <divyanshusharmabb07@gmail.com>
2021-02-24 23:51:35 +0530

    part 3

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
-import numpy
+import numpy 
 import tflearn
 import tensorflow
 import random
@@ -21,13 +21,13 @@ for intent in data['intents']:
     for pattern in intent['patterns']:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs_x.append(pattern)
+        docs_x.append(wrds)
         docs_y.append(intent["tag"])
         
     if intent['tag'] not in labels:
         labels.append(intent['tag'])
 
-words = [stemmer.stem(w.lower()) for w in words]
+words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
 words = sorted(list(set(words)))
 
 labels = sorted(labels)
@@ -35,9 +35,9 @@ labels = sorted(labels)
 training =[]
 output = []
 
-out_empty = [0 for _ in range(len(classes))]
+out_empty = [0 for _ in range(len(labels))]
 
-for x, doc in enumerate(doc_x):
+for x, doc in enumerate(docs_x):
     bag = []
 
     wrds = [stemmer.stem(w) for w in doc]
@@ -49,11 +49,23 @@ for x, doc in enumerate(doc_x):
             bag.append(0)
 
     output_row = out_empty[:]
-    output_row[labels.index(doc_y[x])] = 1
+    output_row[labels.index(docs_y[x])] = 1
 
     training.append(bag)
     output.append(output_row)
 
 training = numpy.array(training)
-output = np.array(output)
+output = numpy.array(output)
 
+# tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape = [None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation = "softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+model.fit(training, output, n_epoch = 1000, batch_size =8, show_metric = True)
+model.save("model.tflearn")
